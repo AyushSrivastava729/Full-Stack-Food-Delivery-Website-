ebc8dadb8386cc3708faf21c4c9e8b16e360b14b
jafrr <126512590+jafrr@users.noreply.github.com>
2025-08-17 14:07:40 +0530

    Update App.jsx

--- admin/admin/src/App.jsx
+++ admin/admin/src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const url = "http://localhost:4000";
+  const url = "https://food-delivery-backend-h677.onrender.com/";
   return (
     <div>    
       <ToastContainer />
